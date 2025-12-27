54e16b3d8c8ac379ad4fac01bc351b43cbc88bd3
kalleva <kaluzhninlev@gmail.com>
2025-12-27 11:51:23 +0100

    First version that allows logging int app.log

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import applog
 import hardware.sht4x as sht4x
+import time
 
 applog.setup()
 logger = logging.getLogger("envmon.main")
@@ -8,8 +9,11 @@ logger = logging.getLogger("envmon.main")
 
 def main():
     logger.info("App started")
-    result = sht4x.get_temperature_and_humidity()
-    logger.info(f"temp: {result.temperature:0.2f}, humid: {result.humidity:0.2f}")
+    while True:
+        result = sht4x.get_temperature_and_humidity()
+        logger.info(f"temp: {result.temperature:0.2f}, humid: {result.humidity:0.2f}")
+        time.sleep(60)
+
 
 
 if __name__ == "__main__":
